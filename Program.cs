97f4be14c7c9c9618229fec8c4f41038e44fc652
ST10278605 <ST10278605@rcconnect.edu.za>
2025-04-03 17:12:00 +0000

    This part consists of UsersInput

--- Program.cs
+++ Program.cs
@@ -18,6 +18,33 @@ namespace AutoCyberChatBot //ST10278605 //Siphesihle Mavuso
             //Display
             LogoDisplay();
 
+            //Users Name
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"\nHello {userName} welcome to the Cybersecurity Awareness Chatbot!");
+
+
+
+            //Chatbot Menu
+            while (true)
+            {
+                Console.WriteLine("\nAsk me about CyberSecurity Topics (type 'exit' to quit):");
+                Console.WriteLine("1. Password Safety\n2. Phishing\n3. Safe Browsing");
+                Console.Write("Your Choice: ");
+                string userChoice = Console.ReadLine().Trim();
+
+
+                if (userChoice.ToLower() == "exit")
+                {
+                    Console.WriteLine("\nStay safe online! Goodbye!");
+                    break;
+                }
+
+                //Respond to User
+                ProcessUserInput(userChoice);
+            }
+
         }
 
         //Greetings
@@ -52,6 +79,25 @@ namespace AutoCyberChatBot //ST10278605 //Siphesihle Mavuso
 
         }
 
+        //Users Name
+        static void ProcessUserInput(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("\n[Password Safety]: Use long, unique passwords with a mixture of letters, numbers, and symbols.");
+                    break;
+                case "2":
+                    Console.WriteLine("\n[Phishing]: Beware of any emails or messages asking for personal information.");
+                    break;
+                case "3":
+                    Console.WriteLine("\n[Safe Browsing]: Only visit secure websites with (https://.co.za) and avoid any clicking unsafe links.");
+                    break;
+                default:
+                    Console.WriteLine("\nI didn't quite understand that. Could you rephrase?");
+                    break;
+            }
+        }
 
     }
 }
