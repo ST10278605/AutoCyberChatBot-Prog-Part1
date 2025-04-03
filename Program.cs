1c1d0b75c7f307bf631b9bc4eaaa29f0b70330a8
ST10278605 <ST10278605@rcconnect.edu.za>
2025-04-03 16:58:19 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace AutoCyberChatBot //ST10278605 //Siphesihle Mavuso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Greetings
+            VoiceGreeting();
+        }
+
+        //Greetings
+        public static void VoiceGreeting()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("greeting.wav");
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[Error playing sound: " + ex.Message + "]");
+            }
+        }
+
+
+    }
+}
